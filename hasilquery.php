4f2ce2473ce1bb6df34067c5109421700effad78
kungfumas <eri299@gmail.com>
2017-03-19 08:40:28 -0700

    versi 1
    menampilkan hasil query

---
+++ hasilquery.php
@@ -0,0 +1,39 @@
+ <?php
+ //https://dev.mysql.com/doc/refman/5.5/en/fulltext-boolean.html
+ //ALTER TABLE dokumen
+//ADD FULLTEXT INDEX `FullText` 
+//(`token` ASC, 
+ //`tokenstem` ASC);
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbstbi";
+$katakunci="";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$hasil=$_POST[katakunci];
+//$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` where token like '%$hasil%'";
+$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` WHERE MATCH (token,tokenstem) AGAINST ('$hasil' IN BOOLEAN MODE)";
+
+
+echo $sql;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Nama file: " . $row["nama_file"]. " - Token: " . $row["token"]. " " . $row["tokenstem"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+///
+
+?>
