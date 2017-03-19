5e2b2a506846c438df981738e579a2ac114f95b6
kungfumas <eri299@gmail.com>
2017-03-19 08:38:56 -0700

    versi 1

---
+++ query.php
@@ -0,0 +1,8 @@
+<html>
+<title>Query</title>
+<body>
+<form enctype="multipart/form-data" method="POST" action="hasilquery.php">
+Keyword : <br>
+<input type="text" name="katakunci"><br>
+<input type=submit>
+</form>
