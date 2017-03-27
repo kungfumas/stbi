db186caef0a34c6073ec33e9fd65f62208a17fd9
kungfumas <eri299@gmail.com>
2017-03-27 10:47:24 +0700

    Versi 1 untuk menghitung tf.idf
    Program untuk menghitung term frequency dan dokumen frequency

---
+++ hitungbobot.php
@@ -0,0 +1,43 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$database='dbstbi';
+
+$conn=mysql_connect($host,$user,$pass);
+mysql_select_db($database);
+//hitung index
+mysql_query("TRUNCATE TABLE tbindex");
+$resn = mysql_query("INSERT INTO `tbindex`(`Term`, `DocId`, `Count`) SELECT `token`,`nama_file`,count(*) FROM `dokumen` group by `nama_file`,token");
+	$n = mysql_num_rows($resn);
+	
+
+//berapa jumlah DocId total?, n
+	$resn = mysql_query("SELECT DISTINCT DocId FROM tbindex");
+	$n = mysql_num_rows($resn);
+	
+	//ambil setiap record dalam tabel tbindex
+	//hitung bobot untuk setiap Term dalam setiap DocId
+	$resBobot = mysql_query("SELECT * FROM tbindex ORDER BY Id");
+	$num_rows = mysql_num_rows($resBobot);
+	print("Terdapat " . $num_rows . " Term yang diberikan bobot. <br />");
+
+	while($rowbobot = mysql_fetch_array($resBobot)) {
+		//$w = tf * log (n/N)
+		$term = $rowbobot['Term'];		
+		$tf = $rowbobot['Count'];
+		$id = $rowbobot['Id'];
+		
+		//berapa jumlah dokumen yang mengandung term tersebut?, N
+		$resNTerm = mysql_query("SELECT Count(*) as N FROM tbindex WHERE Term = '$term'");
+		$rowNTerm = mysql_fetch_array($resNTerm);
+		$NTerm = $rowNTerm['N'];
+		
+		$w = $tf * log($n/$NTerm);
+		
+		//update bobot dari term tersebut
+		$resUpdateBobot = mysql_query("UPDATE tbindex SET Bobot = $w WHERE Id = $id");		
+  	} //end while $rowbobot
+
+
+?>
