3d21d6e869a39db1ada6b3a416b9b946f5b18149
kungfumas <eri299@gmail.com>
2017-04-02 17:48:14 +0700

    awal query tf idf versi 1
    bagian dari stbi

---
+++ awalquery.php
@@ -0,0 +1,8 @@
+<html>
+<title>Form Upload</title>
+<body>
+<form enctype="multipart/form-data" method="POST" action="querytf2.php">
+Kata Kunci : <br>
+  <input type="text" name="keyword"><br>
+<input type=submit value=Submit>
+</form>
