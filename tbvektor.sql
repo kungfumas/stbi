45fe72c6f7697209b569b364aad1c1666828f40e
kungfumas <eri299@gmail.com>
2017-04-02 17:47:01 +0700

    tabel vektor 1 disimpan di database dbstbi

---
+++ tbvektor.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2017 at 12:44 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbstbi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbvektor`
+--
+
+CREATE TABLE `tbvektor` (
+  `DocId` varchar(150) NOT NULL,
+  `Panjang` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbvektor`
+--
+
+INSERT INTO `tbvektor` (`DocId`, `Panjang`) VALUES
+('./files/Perpres0082012.pdf', 60.9969),
+('./files/UU 062014(1).pdf', 12.857),
+('./files/UU 062014(2).pdf', 13.3135),
+('./files/uupangan13.pdf', 76.7842);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbvektor`
+--
+ALTER TABLE `tbvektor`
+  ADD PRIMARY KEY (`DocId`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
