08a8a65b0f3a984e3bc19ec587166a067e661c6e
kungfumas <eri299@gmail.com>
2018-12-17 17:53:01 +0700

    Add files via upload
    analisa sentimen naive bayes

---
+++ analisasentimen.R
@@ -0,0 +1,33 @@
+library(caret)
+library(tm)
+library(SnowballC)
+library(arm)
+# Training data.
+data <- c('Bom mapolres surabaya adalah pengalihan isu.', 'Peledakan bom mapolres surabaya menelan 5 korban jiwa.')
+corpus <- VCorpus(VectorSource(data))
+
+# Create a document term matrix.
+tdm <- DocumentTermMatrix(corpus, list(removePunctuation = TRUE, stopwords = TRUE, stemming = TRUE, removeNumbers = TRUE))
+
+# Convert to a data.frame for training and assign a classification (factor) to each document.
+train <- as.matrix(tdm)
+train <- cbind(train, c(0, 1))
+colnames(train)[ncol(train)] <- 'y'
+train <- as.data.frame(train)
+train$y <- as.factor(train$y)
+data
+train
+# Train.
+fit <- train(y ~ ., data = train, method = 'bayesglm')
+
+# Check accuracy on training.
+predict(fit, newdata = train)
+
+# Test data.
+data2 <- c('Jatuh korban jiwa dalam ledakan bom di mapolres surabaya.')
+corpus <- VCorpus(VectorSource(data2))
+tdm <- DocumentTermMatrix(corpus, control = list(dictionary = Terms(tdm), removePunctuation = TRUE, stopwords = TRUE, stemming = TRUE, removeNumbers = TRUE))
+test <- as.matrix(tdm)
+
+# Check accuracy on test.
+predict(fit, newdata = test)
