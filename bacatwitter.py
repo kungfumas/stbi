bf0d41f5ac67b15ad8a218c8990ef7a2ab840aeb
kungfumas <eri299@gmail.com>
2019-07-04 20:46:56 +0700

    baca dan simpan file dari twitter

---
+++ bacatwitter.py
@@ -0,0 +1,39 @@
+import tweepy
+import nltk
+from tweepy import OAuthHandler
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+
+
+# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+tknzr = TweetTokenizer()
+consumer_key = 'xxxxxxxx'
+consumer_secret = 'xxxxxx'
+access_token = 'xxxxxx'
+access_secret = 'xxxxxxx'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+for status in tweepy.Cursor(api.home_timeline).items(50):
+
+# Process a single status
+
+  # print(status.text)
+   case_folding =status.text.lower()
+  # print(case_folding)
+   output   = stemmer.stem(case_folding)
+   print(output)
+ 
+   #tokens=tknzr.tokenize(output)
+   #print(tokens)
+   
+   file1 = open("MyFile3.txt","a") 
+   file1.write('"'+output+'",')
+   file1.close()
