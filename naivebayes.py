e996af377c82c8bcf17c4ca94b0caaa2dd8025e0
kungfumas <eri299@gmail.com>
2019-11-25 18:23:06 +0700

    update data drive
    update data drive

--- naivebayes.py
+++ naivebayes.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import sys  
+from google.colab import drive
+drive.mount('/gdrive')
 
-spam_data = pd.read_csv('/resources/data/datamerek2.csv')
+spam_data = pd.read_csv('/gdrive/My Drive/datalagu.csv')
 spam_data['label'] = np.where(spam_data['label']=='positif',1,0)
 print(spam_data.shape)
 spam_data.head(10)
